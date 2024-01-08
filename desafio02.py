4df191b1f63da1e5cddef74eb7984269f47857a3
Anaísa Mayara Teodoro <anaisateodoro@hotmail.com>
2024-01-07 23:06:30 -0300

    add exercise 4 and 6

---
+++ desafio02.py
@@ -0,0 +1,5 @@
+print('======Desafio 02 ======')
+dia = input('Qual é a data do seu aniversário? ')
+mes = input('Qual é o mês seu aniversário? ')
+ano = input('Qual é o ano do seu aniversário? ')
+print('Você nasceu no dia', dia,'de',mes,'de',ano,'.Correto?')
---
+++ desafio03.py
@@ -0,0 +1,4 @@
+print('====Desafio 3====')
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+print('A soma é', numero1+ numero2)
